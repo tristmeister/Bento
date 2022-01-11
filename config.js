1cb7c2da7ef77563a85296c9b931fcd584cb0c38
tristmeister <70603517+tristmeister@users.noreply.github.com>
2022-01-11 17:57:25 +0100

    Lat + Long

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ const CONFIG = {
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '53.455714',
+  defaultLongitude: '7.445104',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
